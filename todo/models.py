5a84a94218b507836d5063f492d9024c2173d6d2
Naoto3615 <supra3615@gmail.com>
2025-06-10 14:31:14 +0900

    run migrations

--- todo/models.py
+++ todo/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 
+CHOICES = [
+    ('danger', 'Danger'),
+    ('high',   'High'),
+    ('normal', 'Normal'),
+]
+
 class TodoModel(models.Model):
-    title = models.CharField(max_length=100)
-    memo = models.TextField()
+    title    = models.CharField(max_length=100)
+    memo     = models.TextField()
+    priority = models.CharField(
+        max_length=10,          # max_length必須
+        choices=CHOICES,        # (actual, human-readable)のタプルリスト
+        default='normal'
+    )
+    date = models.DateField()
 
     def __str__(self):
         return self.title
